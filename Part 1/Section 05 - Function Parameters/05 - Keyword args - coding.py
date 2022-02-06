e9b9f19b49fd01f1209512623ffec6fcc14d34cd
Strahinov Yordan Kostadinov <yordankostadinov.strahinov@extern.a1.at>
2022-02-06 14:50:16 +0200

    Section-05-07

--- Part 1/Section 05 - Function Parameters/05 - Keyword args - coding.py
+++ Part 1/Section 05 - Function Parameters/05 - Keyword args - coding.py
@@ -48,3 +48,74 @@ func11(0, 600, d='goooood morning', e='python!')
 
 func11(11, 'm/s', 24, 'mph', d='unladen', e='swallow')
 
+def func(**kwargs):
+    print(kwargs)
+
+func(a=2,b=4)
+
+def func111(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+func111(4,5,a=2,b=4)
+
+def func112(*,d,**kwargs):
+    print(d)
+    print(kwargs)
+
+func112(d=4,f=45,t=56)
+
+# concatenate.py
+def concatenate(**kwargs):
+    result = ""
+    # Iterating over the Python kwargs dictionary
+    for arg in kwargs.values():
+        result += arg
+    return result
+
+print(concatenate(a="Real", b="Python", c="Is", d="Great", e="!"))
+
+
+# sum_integers_args_3.py
+def my_sum(*args):
+    result = 0
+    for x in args:
+        result += x
+    return result
+
+list1 = [1, 2, 3]
+list2 = [4, 5]
+list3 = [6, 7, 8, 9]
+
+print(my_sum(*list1, *list2, *list3))
+
+# extract_list_body.py
+my_list = [1, 2, 3, 4, 5, 6]
+
+a, *b, c = my_list
+
+print(a)
+print(b)
+print(c)
+
+# merging_lists.py
+my_first_list = [1, 2, 3]
+my_second_list = [4, 5, 6]
+my_merged_list = [*my_first_list, *my_second_list]
+
+print(my_merged_list)
+
+my1dict={"a": 1,"b": 7}
+my2dict={"c": 3,"d": 4}
+my_mergerdict = {**my1dict,**my2dict}
+
+print(my_mergerdict)
+
+# merging_dicts.py
+my_first_dict = {"A": 1, "B": 2}
+my_second_dict = {"C": 3, "D": 4}
+my_merged_dict = {**my_first_dict, **my_second_dict}
+
+print(my_merged_dict)
+
+print(help(print()))
---
+++ Part 1/Section 05 - Function Parameters/07 - Putting it all together.py
@@ -0,0 +1,69 @@
+def func(a, b):
+    print(a,b)
+
+func(1,2)
+func('hello','world')
+func(b='world', a='hello')
+
+
+def func1(a,b='world',c='!'):
+    print(a,b,c)
+
+func1('hello')
+func1(a='helloooo',b='great world')
+
+def func2(a, b, *args):
+    print(a)
+    print(b)
+    print(args)
+
+func2(1,2,3,4,5,6)
+func2(1,2,3,'x','r')
+
+def func3(*,a,b):
+    print(a)
+    print(b)
+
+func3(a=1,b=2)
+
+def func4(*,a=1,b):
+    print(a)
+    print(b)
+
+func4(b=2)
+
+def func5(a,b,*,c,d):
+    print(a,b,c,d)
+
+func5(1,2,c=3,d=4,)
+
+def func6(a,b=10,*,c=7,d):
+    print(a,b,c,d)
+
+func6(1,d=4,)
+
+def func7(a,b=10,*args,c=7,d):
+    print(a,b,args,c,d)
+
+func7(1,2,'x','y',d=4,)
+
+def func7(a,b=10,*args,c=7,d,**kwargs):
+    print(a,b,args,c,d,kwargs)
+
+func7(1,'x','y',d=4,f=4,r=6,y=7)
+
+
+print(1,2,3,sep='\t *** \t',end='\t ***')
+
+def calc_hi_lo_avg(*args, log_to_console=False):
+    hi = int(bool(args)) and max(args)
+    lo = int(bool(args)) and min(args)
+    avg = (hi + lo)/2
+    if log_to_console:
+        print("High is {0}, Low is {1}, Avg is {2}".format(hi, lo, avg))
+    return avg
+
+avg = calc_hi_lo_avg(1,2,3,4,5)
+print(avg)
+
+calc_hi_lo_avg(1,2,3,4,5,log_to_console=True)

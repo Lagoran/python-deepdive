7d587842ed3f61a507c248eca1dd6566ef92faaa
Strahinov Yordan Kostadinov <yordankostadinov.strahinov@extern.a1.at>
2022-02-17 11:57:04 +0200

    Section-06-04-01

---
+++ Part 1/Section 06 - First-Class Functions/04-01.py
@@ -0,0 +1,13 @@
+import random
+
+help(random.random)
+
+print(random.random())
+
+l = [1,2,3,4,5,6,7,8,9]
+
+print(sorted(l, key = lambda x : random.random()))
+
+print(sorted('1qwedvcdcdz', key = lambda x : random.random()))
+
+print(''.join(sorted('1qwedvcdcdz', key = lambda x : random.random())))
